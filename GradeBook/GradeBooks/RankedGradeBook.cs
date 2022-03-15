fe781206a506069b3d52ea20d2b332dd99fbe3f7
johnbero <50382247+johnbero@users.noreply.github.com>
2022-03-15 10:26:03 -0500

    Module 3 - Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,15 +1,54 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
 	public class RankedGradeBook : BaseGradeBook
 	{
+		public List<Student> Students { get; set; }
 		public RankedGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Ranked;
+		}
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count < 5)
+			{
+				throw new InvalidOperationException();
+			}
+
+			Students = Students.OrderBy(x => x.AverageGrade).ToList();
+			double topTwentyPercent = Students[GetPercentileValue(80.0)].AverageGrade;
+			double topFourtyPercent = Students[GetPercentileValue(60.0)].AverageGrade;
+			double topSixtyPercent = Students[GetPercentileValue(40.0)].AverageGrade;
+			double topEightyPercent = Students[GetPercentileValue(20.0)].AverageGrade;
+
+			if (averageGrade > topTwentyPercent)
+			{
+				return 'A';
+			}
+			else if (averageGrade <= topTwentyPercent && averageGrade > topFourtyPercent)
+			{
+				return 'B';
+			}
+			else if (averageGrade <= topFourtyPercent && averageGrade > topSixtyPercent)
+			{
+				return 'C';
+			}
+			else if (averageGrade <= topSixtyPercent && averageGrade > topEightyPercent)
+			{
+				return 'D';
+			}
+			return 'F';
+		}
+
+		private int GetPercentileValue(double percentile)
+		{
+			return (int)((percentile / 100) * Students.Count) - 1;
 		}
 	}
 }
