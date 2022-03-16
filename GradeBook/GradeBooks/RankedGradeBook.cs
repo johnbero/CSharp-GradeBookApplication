011f59c588c142153642eccc6075bf17124a64b9
johnbero <50382247+johnbero@users.noreply.github.com>
2022-03-16 08:38:58 -0500

    Module 4 - Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,7 +8,6 @@ namespace GradeBook.GradeBooks
 {
 	public class RankedGradeBook : BaseGradeBook
 	{
-		public List<Student> Students { get; set; }
 		public RankedGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Ranked;
@@ -44,6 +43,28 @@ namespace GradeBook.GradeBooks
 				return 'D';
 			}
 			return 'F';
+		}
+
+		public override void CalculateStatistics()
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+				return;
+			}
+
+			base.CalculateStatistics();
+		}
+
+		public override void CalculateStudentStatistics(string name)
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+				return;
+			}
+
+			base.CalculateStudentStatistics(name);
 		}
 
 		private int GetPercentileValue(double percentile)
